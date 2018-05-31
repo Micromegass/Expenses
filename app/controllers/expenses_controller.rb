149f58b108f09b7d16a06ce368d6a3408efcee83
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-05-31 17:40:49 -0500

    added def new

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -8,11 +8,15 @@ def index
   
   if signed_in? 
   @expenses = current_user.expenses
+  @expense = current_user.expenses.new
   @expenses = Expense.order("date DESC")  
-
   else 
   redirect_to sign_in_path
   end 
+
+   def new
+    @expense = current_user.expenses.new
+ end
 
  
 
