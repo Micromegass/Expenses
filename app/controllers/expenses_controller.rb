42ae440da638c090d459f90a208e817590cc13d2
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-05-31 16:20:33 -0500

    modified search third time

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,7 +5,7 @@ def index
   @expenses = Expense.order("date DESC")
 
   if params[:concept].present?
-  @expenses = @expenses.where('concept LIKE ?', params[:concept])
+    @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
   end 
   if params[:category].present?
   @expenses = @expenses.where('category_id = ?', params[:category_id])
