5b15637d82aad4cf80a2bf511b81120306996991
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-06-01 10:24:46 -0500

    sign_in with devise

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,20 +6,13 @@ before_action :authenticate_user!
 
 def index 
   
-  if signed_in? 
-  @expenses = current_user.expenses
-  @expense = current_user.expenses.new
+  if signed_in?
   @expenses = Expense.order("date DESC")  
   else 
   redirect_to sign_in_path
   end 
 
-   def new
-    @expense = current_user.expenses.new
- end
-
- 
-
 end 
 end 
+
 
